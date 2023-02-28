70424013404d1be9a86ba93030ff2f181975a0d2
Brenton Babb babbb@wit.edu <thebubfusion@gmail.com>
2023-02-28 13:36:02 -0500

    Simplfied primary keys, add not null constraints

--- schema.sql
+++ schema.sql
@@ -1,23 +1,72 @@
+DROP DATABASE photoshare;
+
 CREATE DATABASE IF NOT EXISTS photoshare;
 USE photoshare;
 DROP TABLE IF EXISTS Pictures CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
+CREATE DATABASE IF NOT EXISTS photoshare;
+USE photoshare;
+DROP TABLE IF EXISTS Pictures CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
+
+
 CREATE TABLE Users (
     user_id int4  AUTO_INCREMENT,
-    email varchar(255) UNIQUE,
-    password varchar(255),
-  CONSTRAINT users_pk PRIMARY KEY (user_id)
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    dob DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50),
+    hometown VARCHAR(35),
+    gender VARCHAR(50),
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE Albums(
+    albumid int4 AUTO_INCREMENT,
+    user_id int4,
+    creation_date DATE,
+    name VARCHAR(255),
+    PRIMARY KEY(albumid),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 CREATE TABLE Pictures
 (
-  picture_id int4  AUTO_INCREMENT,
-  user_id int4,
-  imgdata longblob ,
-  caption VARCHAR(255),
-  INDEX upid_idx (user_id),
-  CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
+    picture_id int4  AUTO_INCREMENT,
+    user_id int4,
+    imgdata longblob ,
+    caption VARCHAR(255),
+    album_id int4 NOT NULL,
+    INDEX upid_idx (user_id),
+    PRIMARY KEY (picture_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
-INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
-INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
+
+CREATE TABLE Comments(
+    commentid int4  AUTO_INCREMENT,
+    date DATE,
+    text VARCHAR(255),
+    user_id int4 NOT NULL,
+    picture_id int4 NOT NULL,
+    PRIMARY KEY(commentid),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id)
+);
+
+CREATE TABLE Friends(
+    user_id int4,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Likes(
+    picture_id int4,
+    User_id int4,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id),
+    PRIMARY KEY(user_id, picture_id)
+);
+
+INSERT INTO Users (email, password, dob, first_name) VALUES ('test@bu.edu', 'test', STR_TO_DATE('12-04-2002','%m-%d-%Y'), 'Brenton');
+INSERT INTO Users (email, password, dob, first_name) VALUES ('test2@bu.edu', 'test', STR_TO_DATE('12-04-2002','%m-%d-%Y'), 'Brenton');
