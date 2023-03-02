7e387b28ed78485b589369dee5eb95ab4fe5411c
Brenton Babb babbb@wit.edu <thebubfusion@gmail.com>
2023-03-02 18:10:42 -0500

    Visit posters profile

--- Routing/global_feed.py
+++ Routing/global_feed.py
@@ -13,6 +13,6 @@ global_feed = Blueprint('global_feed', __name__, template_folder='templates')
 @global_feed.route('/global', methods=['GET'])
 def feed():
     cursor = main.conn.cursor()
-    cursor.execute("SELECT imgdata, picture_id, caption FROM Pictures")
+    cursor.execute("SELECT imgdata, picture_id, caption, user_id FROM Pictures")
     photos = cursor.fetchall()
     return render_template('hello.html', message='Global feed', photos=photos, base64=base64)
